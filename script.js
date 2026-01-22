6f81b21ed0547b364c591b878083a9fb66072cbe
Sofia Plaza <sofiiaplaza@hotmail.com>
2026-01-22 23:59:45 +0100

    Image centered, and table elements added in js.

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-const lienzo = document.getElementById("lienzo");
-lienzo.width = 800;
-lienzo.height = 600;
-
-if (lienzo.getContext) {
+window.onload = () => {
+  const lienzo = document.getElementById("lienzo");
+  lienzo.width = 800;
+  lienzo.height = 600;
   const ctx = lienzo.getContext("2d");
 
   ctx.fillStyle = "#5900ff";
   ctx.fillRect(0, 420, 800, 180);
 
-  let img = new Image();
-  img.src = "./inicio.webp";
-  img.onload = function () {
-    ctx.drawImage(img, 171, 80, 458, 443);
-  };
-} else {
-  alert("Not supported");
-}
+  const ilus = [
+    { src: "../inicio.webp", x: 171, y: 80, width: 458, height: 443 },
+    { src: "../recursos/hover-c.svg", x: 460, y: 225, width: 50, height: 60 },
+    { src: "../recursos/hover-a.svg", x: 300, y: 430, width: 180, height: 80 },
+    { src: "../recursos/hover-b.svg", x: 200, y: 265, width: 140, height: 80 },
+    { src: "../recursos/hover-d.svg", x: 400, y: 310, width: 170, height: 125 },
+  ];
 
-function newGame() {
-  draw();
-}
+  function illustrations() {
+    ilus.forEach((ilu) => {
+      const img = new Image();
+      img.onload = () => {
+        ctx.drawImage(img, ilu.x, ilu.y, ilu.width, ilu.height);
+      };
+      img.src = ilu.src;
+    });
+  }
+
+  illustrations();
+};
