ded0b5752f1033d3d97fb52352128722e7fcdde4
Joy <64376365+sofia-pb@users.noreply.github.com>
2026-01-23 02:46:18 +0100

    Update script.js to read images

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ window.onload = () => {
   ctx.fillRect(0, 420, 800, 180);
 
   const ilus = [
-    { src: "../inicio.webp", x: 171, y: 80, width: 458, height: 443 },
-    { src: "../recursos/hover-c.svg", x: 460, y: 225, width: 50, height: 60 },
-    { src: "../recursos/hover-a.svg", x: 300, y: 430, width: 180, height: 80 },
-    { src: "../recursos/hover-b.svg", x: 200, y: 265, width: 140, height: 80 },
-    { src: "../recursos/hover-d.svg", x: 400, y: 310, width: 170, height: 125 },
+    { src: "inicio.webp", x: 171, y: 80, width: 458, height: 443 },
+    { src: "recursos/hover-c.svg", x: 460, y: 225, width: 50, height: 60 },
+    { src: "recursos/hover-a.svg", x: 300, y: 430, width: 180, height: 80 },
+    { src: "recursos/hover-b.svg", x: 200, y: 265, width: 140, height: 80 },
+    { src: "recursos/hover-d.svg", x: 400, y: 310, width: 170, height: 125 },
   ];
 
   function illustrations() {
